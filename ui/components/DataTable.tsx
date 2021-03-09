df12c8e4ce7a1b17a983a8ab09a67d317010ac43
Jordan Pellizzari <jordan@weave.works>
2021-03-09 11:42:25 -0800

    Add empty row styling to DataTable
    Signed-off-by: Jordan Pellizzari <jordan@weave.works>

--- ui/components/DataTable.tsx
+++ ui/components/DataTable.tsx
@@ -15,9 +15,27 @@ type Props = {
   fields: { label: string; value: string | ((k: any) => string) }[];
   rows: any[];
 };
+
+const EmptyRow = styled(TableRow)<{ colSpan: number }>`
+  font-style: italic;
+
+  td {
+    text-align: center;
+  }
+`;
 const Styled = (c) => styled(c)``;
 
 function DataTable({ className, fields, rows }: Props) {
+  const r = _.map(rows, (r, i) => (
+    <TableRow key={i}>
+      {_.map(fields, (f) => (
+        <TableCell key={f.label}>
+          {typeof f.value === "function" ? f.value(r) : r[f.value]}
+        </TableCell>
+      ))}
+    </TableRow>
+  ));
+
   return (
     <div className={className}>
       <TableContainer>
@@ -30,15 +48,15 @@ function DataTable({ className, fields, rows }: Props) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {_.map(rows, (r, i) => (
-              <TableRow key={i}>
-                {_.map(fields, (f) => (
-                  <TableCell key={f.label}>
-                    {typeof f.value === "function" ? f.value(r) : r[f.value]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
+            {r.length > 0 ? (
+              r
+            ) : (
+              <EmptyRow colSpan={fields.length}>
+                <TableCell colSpan={fields.length}>
+                  <span style={{ fontStyle: "italic" }}>No rows</span>
+                </TableCell>
+              </EmptyRow>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
