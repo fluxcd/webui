cb5a0d520da31701d079cbba03e43e2a66cc7be6
Jordan Pellizzari <jordan@weave.works>
2021-03-09 11:42:49 -0800

    Add breadcrumb navigation to pages
    Signed-off-by: Jordan Pellizzari <jordan@weave.works>

--- ui/pages/WorkloadsDetail.tsx
+++ ui/pages/WorkloadsDetail.tsx
@@ -1,7 +1,8 @@
-import { Box } from "@material-ui/core";
+import { Box, Breadcrumbs } from "@material-ui/core";
 import _ from "lodash";
 import * as React from "react";
 import styled from "styled-components";
+import Flex from "../components/Flex";
 import KeyValueTable from "../components/KeyValueTable";
 import Link from "../components/Link";
 import Page from "../components/Page";
@@ -33,47 +34,56 @@ function WorkloadsDetail({ className }: Props) {
   }
 
   return (
-    <div className={className}>
-      <Page>
-        <h2>{workloadDetail.name}</h2>
-        <Panel title="Info">
-          <KeyValueTable
-            columns={[2]}
-            pairs={[
-              {
-                key: "Reconciled By",
-                value: (
-                  <Link
-                    to={formatURL(
-                      PageRoute.KustomizationDetail,
-                      currentContext,
-                      workloadDetail.kustomizationrefnamespace,
-                      { kustomizationId: workloadDetail.kustomizationrefname }
-                    )}
-                  >
-                    {workloadDetail.kustomizationrefname}
-                  </Link>
-                ),
-              },
-            ]}
-          ></KeyValueTable>
+    <Page className={className}>
+      <Breadcrumbs>
+        <Link
+          to={formatURL(PageRoute.Workloads, currentContext, currentNamespace)}
+        >
+          <Flex wide>
+            <h2>Workloads</h2>
+          </Flex>
+        </Link>
+        ,<h2>{workloadDetail.name}</h2>,
+      </Breadcrumbs>
+      <Panel title="Info">
+        <KeyValueTable
+          columns={[2]}
+          pairs={[
+            {
+              key: "Reconciled By",
+              value: workloadDetail.kustomizationrefname ? (
+                <Link
+                  to={formatURL(
+                    PageRoute.KustomizationDetail,
+                    currentContext,
+                    workloadDetail.kustomizationrefnamespace,
+                    { kustomizationId: workloadDetail.kustomizationrefname }
+                  )}
+                >
+                  {workloadDetail.kustomizationrefname}
+                </Link>
+              ) : (
+                "-"
+              ),
+            },
+          ]}
+        ></KeyValueTable>
+      </Panel>
+      <Box paddingTop={2}>
+        <Panel title="Containers">
+          {_.map(workloadDetail.podtemplate.containers, (c) => (
+            <KeyValueTable
+              key={c.name}
+              columns={2}
+              pairs={[
+                { key: "Name", value: c.name },
+                { key: "Image", value: c.image },
+              ]}
+            ></KeyValueTable>
+          ))}
         </Panel>
-        <Box paddingTop={2}>
-          <Panel title="Containers">
-            {_.map(workloadDetail.podtemplate.containers, (c) => (
-              <KeyValueTable
-                key={c.name}
-                columns={2}
-                pairs={[
-                  { key: "Name", value: c.name },
-                  { key: "Image", value: c.image },
-                ]}
-              ></KeyValueTable>
-            ))}
-          </Panel>
-        </Box>
-      </Page>
-    </div>
+      </Box>
+    </Page>
   );
 }
 
