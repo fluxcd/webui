2c7c880e6fda682b5e162362564c4f2c053b13c8
Jordan Pellizzari <jordan@weave.works>
2021-04-07 14:24:39 -0700

    Add backend unit tests
    Signed-off-by: Jordan Pellizzari <jordan@weave.works>

--- pkg/clustersserver/clustersserver_test.go
+++ pkg/clustersserver/clustersserver_test.go
@@ -2,13 +2,18 @@ package clustersserver_test
 
 import (
 	"context"
+	"errors"
 
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
 	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1beta1"
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	pb "github.com/fluxcd/webui/pkg/rpc/clusters"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 var _ = Describe("clustersserver", func() {
@@ -80,5 +85,68 @@ var _ = Describe("clustersserver", func() {
 		Expect(len(res.HelmReleases)).To(Equal(1))
 
 	})
+	DescribeTable("ListSources", func(sourceType pb.Source_Type) {
+		k8sObj, err := getPopulatedSourceType(sourceType)
+
+		Expect(err).NotTo(HaveOccurred())
+
+		err = testclient.Create(context.Background(), k8sObj)
+
+		Expect(err).NotTo(HaveOccurred())
+
+		res, err := c.ListSources(context.Background(), &pb.ListSourcesReq{
+			SourceType: sourceType,
+		})
+
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(len(res.Sources)).To(Equal(1))
+
+	},
+		Entry("git repo", pb.Source_Git),
+		Entry("bucket", pb.Source_Bucket),
+		Entry("helm release", pb.Source_Helm),
+	)
+	It("ListEvents", func() {
+		e := &corev1.Event{ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-event",
+			Namespace: "default",
+		}}
+
+		err = testclient.Create(context.Background(), e)
+		Expect(err).NotTo(HaveOccurred())
+
+		res, err := c.ListEvents(context.Background(), &pb.ListEventsReq{})
+
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(len(res.Events)).To(Equal(1))
+	})
 
 })
+
+func getPopulatedSourceType(sourceType pb.Source_Type) (runtime.Object, error) {
+	objMeta := metav1.ObjectMeta{Name: "somename", Namespace: "default"}
+
+	switch sourceType {
+	case pb.Source_Git:
+		return &sourcev1.GitRepository{
+			ObjectMeta: objMeta,
+			Spec: sourcev1.GitRepositorySpec{
+				URL:       "ssh://git@github.com/someorg/myproj",
+				Reference: &sourcev1.GitRepositoryRef{},
+			},
+		}, nil
+
+	case pb.Source_Bucket:
+		return &sourcev1.Bucket{ObjectMeta: objMeta}, nil
+
+	case pb.Source_Helm:
+		return &sourcev1.HelmRepository{ObjectMeta: objMeta}, nil
+
+	case pb.Source_Chart:
+		return &sourcev1.HelmChart{}, nil
+	}
+
+	return nil, errors.New("could not find source type")
+}
