d34e44eeee23b8711b8ca46ceeb3b0693d163c9f
Jordan Pellizzari <jordan@weave.works>
2021-04-13 11:54:09 -0700

    Convert to go 1.16 file embedding
    Signed-off-by: Jordan Pellizzari <jordan@weave.works>

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/fluxcd/pkg/runtime/logger"
-	"github.com/fluxcd/webui/pkg/assets"
 	"github.com/fluxcd/webui/pkg/clustersserver"
-
-	"k8s.io/client-go/tools/clientcmd"
-
+	"github.com/go-logr/logr"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func init() {
@@ -62,12 +63,73 @@ func main() {
 
 	mux.Handle("/api/clusters/", http.StripPrefix("/api/clusters", clusters))
 
-	mux.Handle("/", http.FileServer(assets.Assets))
+	assetFS := getAssets()
+	assetHandler := http.FileServer(http.FS(assetFS))
+	redirector := createRedirector(assetFS, log)
+
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		extension := filepath.Ext(req.URL.Path)
+		// We use the golang http.FileServer for static file requests.
+		// This will return a 404 on normal page requests, ie /kustomizations and /sources.
+		// Redirect all non-file requests to index.html, where the JS routing will take over.
+		if extension == "" {
+			redirector(w, req)
+			return
+		}
+		assetHandler.ServeHTTP(w, req)
+	}))
 
 	log.Info("Serving on port 9000")
 
 	if err := http.ListenAndServe(":9000", mux); err != nil {
 		log.Error(err, "server exited")
 		os.Exit(1)
+	}
+}
+
+//go:embed dist/*
+var static embed.FS
+
+func getAssets() fs.FS {
+	f, err := fs.Sub(static, "dist")
+
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func createRedirector(fsys fs.FS, log logr.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		indexPage, err := fsys.Open("index.html")
+
+		if err != nil {
+			log.Error(err, "could not open index.html page")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		stat, err := indexPage.Stat()
+		if err != nil {
+			log.Error(err, "could not get index.html stat")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		bt := make([]byte, stat.Size())
+		_, err = indexPage.Read(bt)
+
+		if err != nil {
+			log.Error(err, "could not read index.html")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		_, err = w.Write(bt)
+
+		if err != nil {
+			log.Error(err, "error writing index.html")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 }
