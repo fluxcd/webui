b5d5f624519cfc9239c0d96064c2e86d064763e0
Chanwit Kaewkasi <chanwit@gmail.com>
2021-09-09 16:17:57 +0700

    add code formatter for TypeScript
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- ui/lib/rpc/twirp.ts
+++ ui/lib/rpc/twirp.ts
@@ -1,24 +1,25 @@
-
 export interface TwirpErrorJSON {
-    code: string;
-    msg: string;
-    meta: {[index:string]: string};
+  code: string;
+  msg: string;
+  meta: { [index: string]: string };
 }
 
 export class TwirpError extends Error {
-    code: string;
-    meta: {[index:string]: string};
+  code: string;
+  meta: { [index: string]: string };
 
-    constructor(te: TwirpErrorJSON) {
-        super(te.msg);
+  constructor(te: TwirpErrorJSON) {
+    super(te.msg);
 
-        this.code = te.code;
-        this.meta = te.meta;
-    }
+    this.code = te.code;
+    this.meta = te.meta;
+  }
 }
 
 export const throwTwirpError = (resp: Response) => {
-    return resp.json().then((err: TwirpErrorJSON) => { throw new TwirpError(err); })
+  return resp.json().then((err: TwirpErrorJSON) => {
+    throw new TwirpError(err);
+  });
 };
 
 export const createTwirpRequest = (url: string, body: object, headersOverride: HeadersInit = {}): Request => {
